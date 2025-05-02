e5af9d6c5b72693da5f1f701c91bc1c269cde694
brianna-rhodes <brianna.rhodes@mga.edu>
2025-05-02 09:55:21 -0400

    Update urls.py

--- Library_manage/account/urls.py
+++ Library_manage/account/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
+from .views import CustomLoginView
+from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 
 app_name = 'account'
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('profile/', views.profile, name='profile'),
     path('profile/edit/', views.ProfileUpdateView.as_view(), name='profile_edit'),
+    path('profile/password/', PasswordChangeView.as_view(template_name='account/password_change.html', success_url='done/'), name='password_change'),
+    path('profile/password/done/', PasswordChangeDoneView.as_view(template_name='account/password_change_done.html'), name='password_change_done'),
 ] 
